f2a8183ba680fdc8f57156ff24d533d776ade90c
Jie <jie_cao@hotmail.com>
2020-04-26 00:33:10 +0800

    hash implement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod util;
 use util::hash;
 fn main() {
-    hash::hash_data("Hello, world!");
+    let test_array:[u8;2] = [0xc3, 0x97];
+    let hased_value = hash::hash_data(&test_array, 2, 0xbc9f1d34);
+    println!("{:x}",hased_value);
 }
--- src/util/hash.rs
+++ src/util/hash.rs
@@ -1,7 +1,29 @@
-
-    pub fn hash_data(data:&str) {
-        let u_array:&[u8] = data.as_bytes();
-        for (i, x) in u_array.iter().enumerate() {
-            println!("Item {} = {}", i, x);
+use crate::util::coding;
+    pub fn hash_data(u_array:&[u8], n:i64, seed:u32) -> u32{
+        let m:u32 = 0xc6a4a793;
+        let r:u32 = 24;
+        let mut h:u32 = seed ^ (n * m as i64) as u32;
+        //let u_array:&[u8] = data.as_bytes();
+        let mut index:i64 = 0;
+        while index + 4 < n {
+            let w:u32 = coding::decode_fix_u32(&u_array);
+            index += 4;
+            h += w;
+            h *= m;
+            h ^= h>>16;
         }
+
+        let remain:i64 = index - n;
+        if remain == 3 {
+            h += (u_array[2] << 16) as u32; 
+            h += (u_array[1] << 8) as u32;
+
+        } else if remain == 2 {
+            h += (u_array[1] << 8) as u32;
+        }
+
+        h += u_array[0] as u32;
+        h *= m;
+        h ^= h >> r;
+        return h;
     }
