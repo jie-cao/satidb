46f0d23429b1073436d3863dd36d9b64f88763bc
chexue <chexue.cj@antfin.com>
2020-04-26 15:44:37 +0800

    hash done

--- src/util/coding.rs
+++ src/util/coding.rs
@@ -1,7 +1,7 @@
 
 pub fn decode_fix_u32(data:&[u8]) -> u32{
     return data[0] as u32 
-            | (data[1] << 8) as u32
-            | (data[2] << 16) as u32
-            | (data[3] << 24) as u32; 
+            | (data[1] as u32).wrapping_shl(8)
+            | (data[2] as u32).wrapping_shl(16)
+            | (data[3] as u32).wrapping_shl(24); 
 }
--- src/util/hash.rs
+++ src/util/hash.rs
@@ -1,29 +1,35 @@
 use crate::util::coding;
-    pub fn hash_data(u_array:&[u8], n:i64, seed:u32) -> u32{
-        let m:u32 = 0xc6a4a793;
-        let r:u32 = 24;
-        let mut h:u32 = seed ^ (n * m as i64) as u32;
-        //let u_array:&[u8] = data.as_bytes();
-        let mut index:i64 = 0;
-        while index + 4 < n {
-            let w:u32 = coding::decode_fix_u32(&u_array);
-            index += 4;
-            h += w;
-            h *= m;
-            h ^= h>>16;
-        }
-
-        let remain:i64 = index - n;
-        if remain == 3 {
-            h += (u_array[2] << 16) as u32; 
-            h += (u_array[1] << 8) as u32;
-
-        } else if remain == 2 {
-            h += (u_array[1] << 8) as u32;
-        }
-
-        h += u_array[0] as u32;
-        h *= m;
-        h ^= h >> r;
-        return h;
+pub fn hash_data(u_array: &[u8], n: usize, seed: u32) -> u32 {
+    let m: u32 = 0xc6a4a793;
+    let r: u32 = 24;
+    let mut h: u32 = seed ^ (n * m as usize) as u32;
+    let mut index: usize = 0;
+    
+    while index + 4 <= n {
+        let w: u32 = coding::decode_fix_u32(&u_array[index..index+4]);
+        index += 4;
+        h += w;
+        h = h.wrapping_mul(m);
+        h ^= h >> 16;
     }
+
+    let remain: usize = index - n;
+
+    if remain == 3 {
+        h += (u_array[2] << 16) as u32;
+        h += (u_array[1] << 8) as u32;
+        h += u_array[0] as u32;
+        h = h.wrapping_mul(m);
+        h ^= h >> r;
+    } else if remain == 2 {
+        h += (u_array[1] << 8) as u32;
+        h += u_array[0] as u32;
+        h = h.wrapping_mul(m);
+        h ^= h >> r;
+    } else if remain == 1 {
+        h += u_array[0] as u32;
+        h = h.wrapping_mul(m);
+        h ^= h >> r;
+    }
+    return h;
+}
